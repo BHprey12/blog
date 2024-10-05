d7f47b881e7ead3d1c4d041ec1e59e3c292c7d38
Brey12 <brucehumpreyfabria@gmail.com>
2024-10-05 15:25:48 +0800

    blog

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const router = require("./routers/post.route.js")
+
+const app = express()
+
+app.use(route)
+
+
+app.listen(4000, () => {
+  console.log("server started !")
+})
+  model.post.create({
+    username: "bruce"
+  })
